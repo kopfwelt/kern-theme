3623fb8ae879259e83d99d30f94d1962f47b5640
Samuel <rott.samuel@gmail.com>
2017-01-06 12:19:18 +0100

    add news plugin to test

--- test/helpers/config.js
+++ test/helpers/config.js
@@ -1,5 +1,5 @@
 // const plugins = ['postcss-import', 'postcss-nested', 'postcss-mixins', 'postcss-conditionals', 'postcss-each', 'postcss-for', 'postcss-calc', 'cssnext'];
-const plugins = ['postcss-mixins', 'postcss-nested', 'postcss-for', 'postcss-calc'];
+const plugins = ['postcss-mixins', 'postcss-nested', 'postcss-for', 'postcss-calc', 'postcss-selector-matches', 'postcss-pxtorem'];
 
 
 module.exports = {plugins};
